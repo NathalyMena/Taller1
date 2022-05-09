e3e21442ceb5639b23251ea83270043f21718835
NathalyMR <natamena2001@gmail.com>
2022-05-09 01:18:45 -0500

    Autenticación

--- myshop/app.js
+++ myshop/app.js
@@ -1,7 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { async } from "@firebase/util";
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword} from "firebase/auth";
+
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,37 +23,65 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
+const db = getFirestore(app);
+
 
 const createUserForm = document.getElementById("createUserForm");
-createUserForm.addEventListener("submit", e => {
+createUserForm.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const name = createUserForm.name.value;
   const email = createUserForm.email.value;
   const password = createUserForm.password.value;
 
-  createUser(name, email, password);
+  const newUser = {
+    name,
+    email,
+    password
+  }
+
+ 
+  const userCreated = await createUser(auth, newUser);
+  console.log(userCreated);
+  await addUserToDatabase(db, userCreated.user.uid, newUser);
+
+  
+
 });
 
 
 
-async function createUser(name, email, password){
+async function createUser(auth, { email, password }) {
 
   try {
-   const {User} = await createUserWithEmailAndPassword(auth, email, password);
-   alert('Econet te da la bienvenida')   
+    const  user = await createUserWithEmailAndPassword(auth, email, password);
+    return user;
+    
 
-  }catch(e) {
+  } catch (e) {
 
-    if(e.code === "auth/weak-password"){
+    if (e.code === "auth/weak-password") {
       alert("Tu contraseña debe tener al menos 6 caracteres");
     }
-    if(e.code === "auth/email-already-in-use"){
+    if (e.code === "auth/email-already-in-use") {
       alert("Este correo ya se encuentra registrado");
     }
 
   }
 }
-  
+
+async function addUserToDatabase(db, userId, userInfo) {
+  try {
+
+    await setDoc(doc(db, "users", userId), userInfo);
+
+  } catch (e) {
+    console.log(e);
+
+  }
+}
+
+
+
 
 
