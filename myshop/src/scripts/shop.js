278217befb1867c21ca8c0388d7faee182facda5
NathalyMR <natamena2001@gmail.com>
2022-05-10 21:10:31 -0500

    Filtros

--- myshop/src/scripts/shop.js
+++ myshop/src/scripts/shop.js
@@ -2,15 +2,20 @@ import {db} from "./apps";
 import { getProducts } from "./functions/products";
 
 const productSection = document.getElementById("products");
+const categoryFilter = document.getElementById("material");
+const orderFilter = document.getElementById("order");
 
+let products = [];
 
 async function loadProducts(){
    const firebaseProducts = await getProducts(db);
    productSection.innerHTML = "";
    firebaseProducts.forEach(product => {
        renderProduct(product);
-   })
-   console.log(firebaseProducts);
+   });
+
+   products = firebaseProducts;
+
 
 }
 
@@ -33,5 +38,47 @@ function renderProduct(item) {
 
     productSection.appendChild(product);
 }
+
+function filterBy(){
+    
+    const newCategory = categoryFilter.value;
+    const newOrder = orderFilter.value;
+   
+    let filteredProducts = [];
+
+    if (newCategory !== ""){
+
+        filteredProducts = products.filter((product) => product.category === newCategory);    
+        
+    } else {
+        filteredProducts = products;
+        
+    }
+
+    if (newOrder === "desc"){
+        filteredProducts = filteredProducts.sort((a,b) => a.price - b.price);
+    }
+
+    if (newOrder === "asc"){
+        filteredProducts = filteredProducts.sort((a,b) => b.price - a.price);
+    }
+
+    productSection.innerHTML= "";
+    filteredProducts.forEach(product => {
+        renderProduct(product);
+    });
+
+   
+
+}
+
+categoryFilter.addEventListener("change", e => {
+   filterBy();
+});
+
+orderFilter.addEventListener("change", e => {
+    filterBy();
+ })
+
 
 loadProducts();
