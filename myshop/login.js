ad08129460d1a71ad8c1cf3ddc2ff0a05af393f0
NathalyMR <natamena2001@gmail.com>
2022-05-15 20:51:59 -0500

    AuthLogin

--- myshop/login.js
+++ myshop/login.js
@@ -1,72 +1,64 @@
 // Import the functions you need from the SDKs you need
-
+import { async } from "@firebase/util";
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-import firebaseConfig from "./src/utils/firebase";
+const firebaseConfig = {
+    apiKey: "AIzaSyBasLyACbbozQFnknmHsnNE5KiRegaotTc",
+    authDomain: "econet-28ee4.firebaseapp.com",
+    projectId: "econet-28ee4",
+    storageBucket: "econet-28ee4.appspot.com",
+    messagingSenderId: "127437720478",
+    appId: "1:127437720478:web:1756b263e6878cb202648f"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
-const db = getFirestore(app);
 
 
 
-const loginForm = document.getElementById("loginForm");
-
-const createUserForm = document.getElementById("createUserForm");
-createUserForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const name = createUserForm.name.value;
-  const email = createUserForm.email.value;
-  const password = createUserForm.password.value;
-
-  const newUser = {
-    name,
-    email,
-    password,
-    isAdmin: false,
-  }
-
- 
-  const userCreated = await createUser(auth, newUser);
- 
-  await addUserToDatabase(db, userCreated.uid, newUser);
-
-  
-
-});
-
-loginForm.addEventListener("submit", e => {
+loginForm.addEventListener("submit", async e => {
     e.preventDefault();
 
     const email = loginForm.email.value;
+    console.log(email);
     const password = loginForm.password.value;
 
-    login( auth, email, password);
-   if (user.isAdmin) {
-        location.href = "./createProduct.html";
+    await login(email, password);
 
-    }else {
-        location.href = "./product.html";
-    }
+
+
+
 });
 
 
 async function login(email, password) {
+
     try {
-        const {user}  = await signInWithEmailAndPassword(auth, email, password);
-        
+        const response = await signInWithEmailAndPassword(auth, email, password);
+        user = response.user;
+        localStorage.setItem("idUser", user.uid)
+
+
+        if (email === "natamena2001@gmail.com") {
+            location.href = "./createProduct.html";
+        } else {
+            location.href = "./shopLogin.html";
+        }
+
+
     } catch (e) {
         alert("Correo o contraseña inválida");
-}
+    }
+
+
+
+
 
 }
+
--- myshop/src/scripts/checkout.js
+++ myshop/src/scripts/checkout.js
@@ -1,26 +1,25 @@
-/* import { auth, db } from "../scripts/apps";
-import { onAuthStateChanged } from "firebase/auth";
-import { getFirebaseCart, createFirebaseCart } from "./functions/functionCart.js";
-import { addProductToCart } from "../utils";
-import { getMyLocalCart, currencyFormat } from "../utils/index";
+import { db } from "./apps";
+import { doc, getDoc } from "firebase/firestore";
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-const db = getFirestore(app);
 
-const createOrder = async (userFields) => {
+async function getUser(id) {
+
+    const docRef = doc(db, "users", id);
+
     try {
-        const order = await addDoc(collection(db, "orders"), {
-            ...userFields, // spread 
-            products: cart,
-            total,
-            email: userLogged.email,
-            status: "pending",
-        });
-        alert(`Muchas gracias, tu pedido con ID: ${order.id} ha quedado registrado.`);
 
-        deleteCart();
-    } catch (e) {
-        console.log(e)
+        const docSnap = await getDoc(docRef);
+        const user = docSnap.data();
+        console.log(user)
+        document.getElementById("nombreUsuario").innerText=user.name;
+
+        return user;
+
+    }catch(e){
+        console.log(e);
     }
-};*/
+}
+
+
+const id = localStorage.getItem("idUser");
+getUser(id);
